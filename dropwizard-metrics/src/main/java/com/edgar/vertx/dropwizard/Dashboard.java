b76e9e07518c03214b30c711e2c106e111f6f8c4
edgar615 <edgar615@gmail.com>
2017-03-06 17:53:54 +0800

    metric

--- dropwizard-metrics/src/main/java/com/edgar/vertx/dropwizard/Dashboard.java
+++ dropwizard-metrics/src/main/java/com/edgar/vertx/dropwizard/Dashboard.java
@@ -2,8 +2,12 @@ package com.edgar.vertx.dropwizard;
 
 import com.edgar.util.vertx.runner.Runner;
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Vertx;
+import io.vertx.core.VertxOptions;
 import io.vertx.core.http.HttpServer;
 import io.vertx.core.json.JsonObject;
+import io.vertx.core.metrics.MetricsOptions;
+import io.vertx.ext.dropwizard.DropwizardMetricsOptions;
 import io.vertx.ext.dropwizard.MetricsService;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.StaticHandler;
@@ -12,6 +16,7 @@ import io.vertx.ext.web.handler.sockjs.PermittedOptions;
 import io.vertx.ext.web.handler.sockjs.SockJSHandler;
 
 import java.util.Random;
+import java.util.Set;
 
 /**
  * @author <a href="http://tfox.org">Tim Fox</a>
@@ -19,36 +24,55 @@ import java.util.Random;
 public class Dashboard extends AbstractVerticle {
   // Convenience method so you can run it in your IDE
   public static void main(String[] args) {
-    Runner.runExample(Dashboard.class);
+    Vertx.vertx(new VertxOptions()
+                        .setMetricsOptions(new MetricsOptions().setEnabled(true)))
+    .deployVerticle(Dashboard.class.getName());
   }
 
   @Override
   public void start() {
+
     MetricsService service = MetricsService.create(vertx);
+
     Router router = Router.router(vertx);
-// Allow outbound traffic to the news-feed address
+
+    // Allow outbound traffic to the news-feed address
+
     BridgeOptions options = new BridgeOptions().
             addOutboundPermitted(
                     new PermittedOptions().
                             setAddress("metrics")
             );
+
     router.route("/eventbus/*").handler(SockJSHandler.create(vertx).bridge(options));
-// Serve the static resources
+
+    // Serve the static resources
     router.route().handler(StaticHandler.create());
+
     HttpServer httpServer = vertx.createHttpServer();
     httpServer.requestHandler(router::accept).listen(8080);
-// Send a metrics events every second
+
+    // Send a metrics events every second
     vertx.setPeriodic(1000, t -> {
       JsonObject metrics = service.getMetricsSnapshot(vertx.eventBus());
       vertx.eventBus().publish("metrics", metrics);
+      JsonObject metrics2 = service.getMetricsSnapshot("vertx.eventbus.messages.received");
+      System.out.println(metrics2);
+
+//      Set<String> metricsNames = service.metricsNames();
+//      for (String metricsName : metricsNames) {
+//        System.out.println("Known metrics name " + metricsName);
+//      }
     });
-// Send some messages
+
+    // Send some messages
     Random random = new Random();
     vertx.eventBus().consumer("whatever", msg -> {
       vertx.setTimer(10 + random.nextInt(50), id -> {
         vertx.eventBus().send("whatever", "hello");
       });
     });
-    vertx.eventBus().send("whatever", "hello");
+//    vertx.eventBus().send("whatever", "hello");
   }
+
 }
