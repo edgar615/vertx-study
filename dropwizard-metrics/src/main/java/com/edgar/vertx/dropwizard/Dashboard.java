0cf00d62a56274a82b6dd12ae33a35ef540bd884
edgar615 <edgar615@gmail.com>
2017-03-07 14:49:36 +0800

    share

--- dropwizard-metrics/src/main/java/com/edgar/vertx/dropwizard/Dashboard.java
+++ dropwizard-metrics/src/main/java/com/edgar/vertx/dropwizard/Dashboard.java
@@ -1,5 +1,7 @@
 package com.edgar.vertx.dropwizard;
 
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.SharedMetricRegistries;
 import com.edgar.util.vertx.runner.Runner;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Vertx;
@@ -9,6 +11,7 @@ import io.vertx.core.json.JsonObject;
 import io.vertx.core.metrics.MetricsOptions;
 import io.vertx.ext.dropwizard.DropwizardMetricsOptions;
 import io.vertx.ext.dropwizard.MetricsService;
+import io.vertx.ext.dropwizard.impl.JVMMetricsImpl;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.StaticHandler;
 import io.vertx.ext.web.handler.sockjs.BridgeOptions;
@@ -25,7 +28,8 @@ public class Dashboard extends AbstractVerticle {
   // Convenience method so you can run it in your IDE
   public static void main(String[] args) {
     Vertx.vertx(new VertxOptions()
-                        .setMetricsOptions(new MetricsOptions().setEnabled(true)))
+                        .setMetricsOptions(new MetricsOptions(new JsonObject().put
+                                ("registryName", "my-registry")).setEnabled(true)))
     .deployVerticle(Dashboard.class.getName());
   }
 
@@ -52,13 +56,18 @@ public class Dashboard extends AbstractVerticle {
     HttpServer httpServer = vertx.createHttpServer();
     httpServer.requestHandler(router::accept).listen(8080);
 
+    MetricRegistry registry = SharedMetricRegistries.getOrCreate("my-registry");
+
+    JVMMetricsImpl jvmMetrics = new JVMMetricsImpl(registry, "vertx");
+
     // Send a metrics events every second
     vertx.setPeriodic(1000, t -> {
       JsonObject metrics = service.getMetricsSnapshot(vertx.eventBus());
       vertx.eventBus().publish("metrics", metrics);
-      JsonObject metrics2 = service.getMetricsSnapshot("vertx.eventbus.messages.received");
+//      JsonObject metrics2 = service.getMetricsSnapshot("vertx.eventbus.messages.received");
+//      System.out.println(metrics2);
+      JsonObject metrics2 = service.getMetricsSnapshot("vertx.jvm.memory");
       System.out.println(metrics2);
-
 //      Set<String> metricsNames = service.metricsNames();
 //      for (String metricsName : metricsNames) {
 //        System.out.println("Known metrics name " + metricsName);
