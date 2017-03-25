9694493d7412c3de13299dbb3143ce4bfc814231
Edgar <edgar615@gmail.com>
2017-03-25 18:02:03 +0800

    handlebars

---
+++ vertx-web-handlebar/src/main/java/com/edgar/vertx/web/handlebar/StaticServer.java
@@ -0,0 +1,24 @@
+package com.edgar.vertx.web.handlebar;
+
+import com.edgar.util.vertx.runner.Runner;
+import io.vertx.core.AbstractVerticle;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.StaticHandler;
+import io.vertx.ext.web.templ.HandlebarsTemplateEngine;
+import io.vertx.ext.web.templ.TemplateEngine;
+
+public class StaticServer extends AbstractVerticle {
+
+    public static void main(String[] args) {
+        Runner.runExample(StaticServer.class);
+    }
+
+    @Override
+    public void start() throws Exception {
+//   不经过后端渲染，全部通过前端操作，这个其实是handlebars的学习
+
+      Router router = Router.router(vertx);
+      router.route("/static/*").handler(StaticHandler.create());
+        vertx.createHttpServer().requestHandler(router::accept).listen(8080);
+    }
+}
