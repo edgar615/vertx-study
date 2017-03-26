e82527eb956555778c8c81f05e624a6f3b384389
Edgar <edgar615@gmail.com>
2017-03-26 21:17:16 +0800

    handlebars

--- vertx-web-handlebar/src/main/java/com/edgar/vertx/web/handlebar/StaticServer.java
+++ vertx-web-handlebar/src/main/java/com/edgar/vertx/web/handlebar/StaticServer.java
@@ -16,7 +16,7 @@ public class StaticServer extends AbstractVerticle {
     @Override
     public void start() throws Exception {
 //   不经过后端渲染，全部通过前端操作，这个其实是handlebars的学习
-
+//http://javascriptissexy.com/handlebars-js-tutorial-learn-everything-about-handlebars-js-javascript-templating
       Router router = Router.router(vertx);
       router.route("/static/*").handler(StaticHandler.create());
         vertx.createHttpServer().requestHandler(router::accept).listen(8080);
